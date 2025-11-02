fc89272a3b5a7ef34f2c7f6311ffe0fe85f0120d
RobsonBPA <robsonpasetto1705@gmail.com>
2025-11-02 12:00:48 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    // OpenSans
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                    // Lexend
+                    fonts.AddFont("Lexend-Regular.ttf", "Lexend-Regular");
+                    fonts.AddFont("Lexend-SemiBold.ttf", "Lexend-SemiBold");
+                    fonts.AddFont("Lexend-Bold.ttf", "Lexend-Bold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}

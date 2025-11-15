1d836ef9439d3eedd0b07676927bbfaf6bec81cd
RobsonBPA <robsonpasetto1705@gmail.com>
2025-11-15 08:32:15 -0300

    Adicionado o cálculo do valor total e alterações gráficas realizadas.

--- AppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ AppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using AppHotel.Models;
+
 namespace AppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -24,14 +26,26 @@ public partial class ContratacaoHospedagem : ContentPage
         Navigation.PushAsync(new Sobre());
     }
 
-    private void Button_Clicked_1(object sender, EventArgs e)
+    private async void Button_Clicked_1(object sender, EventArgs e)
     {
         try
         {
-            Navigation.PushAsync(new HospedagemContratada());
+            Hospedagem h = new Hospedagem()
+            {
+                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+                QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                DataCheckIn = dtpck_checkin.Date,
+                DataCheckOut = dtpck_checkout.Date,
+            };
+
+            await Navigation.PushAsync(new HospedagemContratada()
+            {
+                BindingContext = h
+            });
         } catch (Exception ex)
         {
-            DisplayAlert("Ops", ex.Message, "OK");
+            await DisplayAlert("Ops", ex.Message, "OK");
         }
     }
 
